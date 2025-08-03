6aac13e7d8b9cfb03a37ebb0dd229e53a8fcbf99
Daksh Goel <dakshgoel@Dakshs-MacBook-Air.local>
2025-08-03 20:14:35 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // cloudinary url
+        required: true
+    },
+    coverImage: {
+        type: String, // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")){
+        return next()
+    } 
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+}) // save hone se phele kuch karvana ho toh kaam ata hai
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id, // payloadname: database se joh ara hai
+            email: this.email, // payloadname: database se joh ara hai
+            username: this.username, // payloadname: database se joh ara hai
+            fullName: this.fullName // payloadname: database se joh ara hai
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
+
+// if zyada searching hone wali hai koi cheej ki toh usme index daldo taki database ki searching mei ane lag jaye
